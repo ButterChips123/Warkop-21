27a4cbc0d1961e0e0846a097234f09a3ae36bf3b
Chippy <113273520+ButterChips123@users.noreply.github.com>
2025-06-03 10:26:18 +0700

    Add files via upload

---
+++ data ulasan.js
@@ -0,0 +1,27 @@
+// data ulasan.js
+const reviewsData = [
+  {
+    name: "Raka Giri",
+    rating: 5,
+    comment: "Tempatnya cozy dan murah! Cocok banget buat nongkrong bareng temen-temen. Menu-nya juga enak-enak.",
+    date: "12/04/2025",
+  },
+  {
+    name: "Nina A.",
+    rating: 4,
+    comment: "Nongkrong sambil live TikTok disini asik juga, pol pokoknya!",
+    date: "15/04/2025",
+  },
+  {
+    name: "Dimas Putra",
+    rating: 5,
+    comment: "Nasi Goreng-nya enak banget, lo semua harus cobain pokoknya.",
+    date: "20/04/2025",
+  },
+  {
+    name: "Putri Ramadhani I.",
+    rating: 4,
+    comment: "Lumayan lah, kemarin bingung mau nongkrong dimana sama bestie, kebetulan liat Warkop21, jadi tertarik deh.",
+    date: "25/04/2025",
+  },
+];
